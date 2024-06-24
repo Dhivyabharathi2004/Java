d854fb7930dfbed32dc7a86daa557d12725227a0
DHIVYABHARATHI <166492750+Dhivyabharathi2004@users.noreply.github.com>
2024-06-24 18:43:41 +0530

    set

---
+++ set.java
@@ -0,0 +1,12 @@
+import java.util.LinkedList;
+public class set {
+    public static void main(String [] args){
+        LinkedList<String> name = new LinkedList<>();
+        name.add("Dhivya");
+        name.add("Hema");
+        name.add("DD");
+        name.set(2,"Grey");
+        System.out.println(name);
+    }
+    
+}
