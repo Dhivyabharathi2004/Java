7303d62fac719ff3f0865c76d2fec29a65367e16
DHIVYABHARATHI <166492750+Dhivyabharathi2004@users.noreply.github.com>
2024-06-24 18:06:23 +0530

    clearall

---
+++ clearing.java
@@ -0,0 +1,16 @@
+import java.util.LinkedList;
+public class clearing{
+    public static void main(String [] args){
+        LinkedList<String> name = new LinkedList<>();
+        name.add("Dhivya");
+        name.add("Hema");
+        name.add("DD");
+        name.clear();
+        System.out.println(name);
+
+
+
+
+
+    }
+}
