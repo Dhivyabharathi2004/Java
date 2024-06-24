a1df095fb1c5cf96fb107dbc3a6a14ad0c9528e0
DHIVYABHARATHI <166492750+Dhivyabharathi2004@users.noreply.github.com>
2024-06-24 16:07:01 +0530

    list

---
+++ linkedlist.java
@@ -0,0 +1,11 @@
+import java.util.LinkedList;
+class linkedlist{
+    public static void main(String args[]){
+        LinkedList<String> list = new LinkedList<>();
+        list.add("Dhivya");
+        list.add("Dhivyabharathi");
+        list.add("Dhivyadharshini");
+        list.add("Hema");
+        System.out.println(list);
+    }
+}
