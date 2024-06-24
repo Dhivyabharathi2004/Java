a027eba24938670e1b9a0ced2ba5e3240cd32d8b
DHIVYABHARATHI <166492750+Dhivyabharathi2004@users.noreply.github.com>
2024-06-24 18:43:14 +0530

    addlast

---
+++ addlast.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+
+public class addlast {
+public static void main(String[] args) {
+    LinkedList<String> name = new LinkedList<>();
+    name.add("Dhivya");
+        name.add("Hema");
+        name.add("DD");
+        name.addLast("Dhivyabharathi");
+        System.out.println(name);
+        
+}    
+}
